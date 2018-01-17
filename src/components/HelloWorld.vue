f21d782138f985fa968cc6805a42d40762e115be
ChangeWorldByCoding <774131720@qq.com>
2018-01-17 15:48:46 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-row>
+    <el-col :span="24"><el-input v-model="input" placeholder="请输入内容"></el-input></el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
