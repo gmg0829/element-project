35aad2a6c545228c445dab506e4f9c7f522ff5a3
ChangeWorldByCoding <774131720@qq.com>
2018-01-17 17:27:02 +0800

    登陆

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,88 @@
 <template>
-  <el-row>
-    <el-col :span="24"><el-input v-model="input" placeholder="请输入内容"></el-input></el-col>
-  </el-row>
+  <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-position="left" class="login-form">
+    <div class="title-container">
+      <h3 class="title">登陆界面</h3>
+    </div>
+    <el-form-item  prop="name">
+      <el-input v-model="ruleForm.name"  placeholder="请输入用户名"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="ruleForm.password"  placeholder="请输入密码"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPassword">
+      <el-input type="password" v-model="ruleForm.checkPass"  placeholder="确认密码"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')" style="width: 100%">登陆</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          name: '',
+          password: '',
+          checkPass: ''
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPassword: [
+            { validator: validatePass2, trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .login-form {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 650px;
+    padding: 15px 35px 15px 35px;
+    margin: 120px auto;
+  }
 </style>
